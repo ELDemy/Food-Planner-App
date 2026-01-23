2a4bcbdecb67efe3b4f793624138e0c57559c174
ELDemy <mahmoudeldemerdash5@gmail.com>
2026-01-24 00:19:38 +0200

    create meals card , meals list

---
+++ app/src/main/java/com/dmy/foodplannerapp/MealsListScreenFragment.java
@@ -0,0 +1,23 @@
+package com.dmy.foodplannerapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MealsListScreenFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_meals_list_screen, container, false);
+    }
+}
