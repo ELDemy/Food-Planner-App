287d35b01b7c2c1a50eda4d1ad96bdc9c6931647
ELDemy <mahmoudeldemerdash5@gmail.com>
2026-01-24 03:06:47 +0200

    implement the signout, change the inactive icon colod in the nav bar

--- app/src/main/java/com/dmy/foodplannerapp/presentation/profile/view/ProfileFragment.java
+++ app/src/main/java/com/dmy/foodplannerapp/presentation/profile/view/ProfileFragment.java
@@ -1,15 +1,22 @@
 package com.dmy.foodplannerapp.presentation.profile.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.dmy.foodplannerapp.R;
+import com.dmy.foodplannerapp.presentation.auth.view.AuthActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
+    CardView signOutBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,20 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        signOutBtn = view.findViewById(R.id.card_logout);
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View btnView) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), AuthActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
     }
 }
