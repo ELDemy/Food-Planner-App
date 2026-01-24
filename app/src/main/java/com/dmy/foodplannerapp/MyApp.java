381e5d8af17dc20e0c08967aaa12569c61bab12f
ELDemy <mahmoudeldemerdash5@gmail.com>
2026-01-24 11:07:43 +0200

    why MyApp class

--- app/src/main/java/com/dmy/foodplannerapp/MyApp.java
+++ app/src/main/java/com/dmy/foodplannerapp/MyApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import androidx.appcompat.app.AppCompatDelegate;
 
+// this class is to remove the night mode from the app
 public class MyApp extends Application {
 
     @Override
